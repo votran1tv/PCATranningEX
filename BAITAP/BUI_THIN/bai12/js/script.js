d89c3f54c11c1187986b093908be30cc1f7ad9a0
thanhduong9613 <thanhduong9613@gmail.com>
2018-08-01 15:30:58 +0700

    update

--- BAITAP/BUI_THIN/bai12/js/script.js
+++ BAITAP/BUI_THIN/bai12/js/script.js
@@ -29,5 +29,42 @@ jQuery(document).ready(function($) {
 		}
 	}
 	window.addEventListener('scroll', stickyNavigation); 
+$('#weather_location').change(function(){
+	// $('#show').text($(this).find(":selected").text()); /*Lấy text của Option được chọn trong thẻ Select*/
+	if($(this).val() === 'tphochiminh'){
+		$(".nhietdo").text("30°");
+		$(".temp").text("29° - 35°");
+		$(".info").text("Có nắng nhẹ");
+		$(".img_info").attr('src', 'images/img_info.png');
+		$("#test").attr('src', '\images/img_weather2.png');
+	}
+	else if($(this).val() === 'danang'){
+		$(".nhietdo").text("26°");
+		$(".temp").text("24° - 29°");
+		$(".info").text("Mưa rải rác");
+		$(".img_info").attr('src', 'images/img_info3.png');
+		$("#test").attr('src', 'images/img_weather3.png');
+	}
+	else
+	{
+		$(".nhietdo").text("27°");
+		$(".temp").text("25° - 32°");
+		$(".info").text("Mây mù");
+		$(".img_info").attr('src', 'images/img_info2.png');
+		$("#test").attr('src', '\images/img_weather.png');
+	}
 });
-
+function locationChange(obj){
+	var message = document.getElementById('show');
+    var value = obj.value;
+	if (value === 'nam'){
+        message.innerHTML = "Bạn đã chọn giới tính nam";
+    }
+    else if (value === 'nu'){
+        message.innerHTML = "Bạn đã chọn giới tính nữ";
+    }
+});
+// function a(){
+// 	var x = document.getElementById('weather_location').value;
+// 	document.getElementById("show").innerHTML = x;
+// }
--- BAITAP/THANH_DUONG/news-website/script/script-writer.js
+++ BAITAP/THANH_DUONG/news-website/script/script-writer.js
@@ -1,3 +1,102 @@
+<<<<<<< HEAD
+$(document).ready(function () {
+    var reg = /^([A-Za-z0-9\.])+@([A-Za-z\.]+)\.([A-Za-z]{3,4})/i;
+
+    var title = $('input[name="title"]');
+    var link = $('input[name="link"]');
+    var content = $('textarea[name="content"]');
+    var email = $('input[name="email"]');
+    var tel = $('input[name="tel"]');
+    var tag = $('input[name="tagName"]');
+
+
+    title.change(function(){
+        // console.log(title.val());
+        var conv = convert(title.val());
+        // alert(this.val());
+        // console.log(conv);
+        var t = window.location.href;
+        
+        t = t.replace('file:///mnt/DATA/dao_tao_pca/PCA_EX/','');
+        t = t.replace('/tao-bai-viet.html','/');
+        // console.log(t+conv);
+        if(title.val() == ""){
+            link.val('');
+        }else{
+            link.val(t+conv+'.html');
+        }
+    });
+
+
+    $('#form').submit(function (e) {
+        e.preventDefault();
+        if(title.val() == ""){
+            alert("Bài viết phải có tiêu đề " );
+            title.focus();
+        }else if(content.val().length < 200){
+            alert("Nội dung phải chứa ít nhất 200 kí tự " );    
+            content.focus();
+        }else{
+            if(email.val() == "" || tel.val() == ""){
+                alert("Hãy điền đầy đủ thông tin vào form bên phải");
+            }else if(!reg.test(email.val())){
+                alert("Định dạng email không đúng")
+                email.focus();
+            }else if(isNaN(tel.val())){
+                alert("Số điện thoại không được nhập chữ");
+                tel.focus();
+            }else{
+                // TODO
+                
+            }
+        }
+    });
+
+    $('input[name="addTag"]').on('click',function(){
+        var getTag = tag.val();
+        var arrTag = getTag.split(",");
+        var show ="";
+        if(getTag != ""){
+            for(var i=0;i<arrTag.length;i++){
+                show += '<span class="elTag" style="margin-right:10px;"><b class="delTag"></b>'+arrTag[i].trim()+'</span>';
+                
+            }
+        }
+        
+        $('#showTag').html($('#showTag').html()+show);
+        tag.val("");
+        // remove tag
+        $('.elTag').on('click',function(){
+            $(this).remove();
+        });
+    });
+    
+    $('input[name="ld"]').on('click', function () {
+
+        if($(this).val() == "1"){
+            $('input[name="setTime"]').prop ('disabled', false);
+        }else{
+            $('input[name="setTime"]').prop ('disabled', true);
+        }
+        
+    });
+    $('#reset').on('click',function(){
+        $('#showTag').html("");
+    });
+    
+});
+
+function convert(s){
+    s = s.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/gi, 'a');
+    s = s.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/gi,'e');
+    s = s.replace(/(ì|í|ị|ỉ|ĩ)/gi,'i');
+    s = s.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/gi,'o');
+    s = s.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/gi,'u');
+    s = s.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/gi,'y');
+    s = s.replace(/(đ)/gi,'d');
+    s = s.replace(/\ /g,"-");
+    return s;
+=======
 $(document).ready(function () {
     var reg = /^([A-Za-z0-9\.])+@([A-Za-z\.]+)\.([A-Za-z]{3,4})/i;
 
@@ -85,4 +184,5 @@ function convert(s){
     s = s.replace(/(đ)/gi,'d');
     s = s.replace(/\ /g,"-");
     return s;
+>>>>>>> upstream/master
 }
