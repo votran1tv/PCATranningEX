54d21cbe49e66c44d1ae5e7bc8e906c60faea724
BT <buicongthinit@gmail.com>
2018-08-01 14:23:06 +0700

    Update for Review

--- BAITAP/BUI_THIN/bai3/js/jquery.js
+++ BAITAP/BUI_THIN/bai3/js/jquery.js
@@ -15,7 +15,8 @@ $(document).ready(function(){
             sdt: {
                 required: true,
                 minlength: 6,
-                maxlength: 15
+                maxlength: 15,
+                digits: true
             }
         },
         messages:{
@@ -31,14 +32,42 @@ $(document).ready(function(){
             sdt:{
                 required: "Vui lòng nhập số điện thoại!",
                 minlength: "Số điện thoại đã nhập không đúng!",
-                maxlength: "Số điện thoại đã nhập không đúng!"
+                maxlength: "Số điện thoại đã nhập không đúng!",
+                digits: "Số điện thoại chỉ bao gồm các số nguyên dương từ 0 đến 9"
             }
         }
     });
 
 
 /*Khi đưa con trỏ chuột ra khỏi ô ipTieuDe thì mã hóa các ký tự tiếng Việt có dấu (sử dụng biểu thức chính quy) và đưa chúng vào .linklienket*/    
-    $(".ipTieuDe").blur(function(){
+    // $(".ipTieuDe").blur(function(){
+    //     var a = $(this).val();
+    //     a = a.replace(/à|á|ả|ã|ạ|ă|ằ|ắ|ẳ|ẵ|ặ|â|ầ|ấ|ẩ|ẫ|ậ/igm,"a");
+    //     a = a.replace(/đ/igm, "d");
+    //     a = a.replace(/è|é|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/igm, "e");
+    //     a = a.replace(/ì|í|ỉ|ĩ|ị/igm,"i");
+    //     a = a.replace(/ò|ó|ỏ|õ|ọ|ô|ồ|ố|ổ|ỗ|ộ|ơ|ờ|ớ|ở|ỡ|ợ/igm, "o");
+    //     a = a.replace(/ù|ú|ủ|ũ|ụ|ư|ừ|ứ|ử|ữ|ự/igm,"u");
+    //     a = a.replace(/\ /igm,"-");
+    //     $(".linklienket").text("http://pca.net.vn/" + a);
+    // })
+
+/*Test KeyUp*/
+    $(".ipTieuDe").keyup(function(q){
+        if(q.keyCode == 8){
+            var a = $(this).val();
+            a = a.replace(/à|á|ả|ã|ạ|ă|ằ|ắ|ẳ|ẵ|ặ|â|ầ|ấ|ẩ|ẫ|ậ/igm,"a");
+            a = a.replace(/đ/igm, "d");
+            a = a.replace(/è|é|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/igm, "e");
+            a = a.replace(/ì|í|ỉ|ĩ|ị/igm,"i");
+            a = a.replace(/ò|ó|ỏ|õ|ọ|ô|ồ|ố|ổ|ỗ|ộ|ơ|ờ|ớ|ở|ỡ|ợ/igm, "o");
+            a = a.replace(/ù|ú|ủ|ũ|ụ|ư|ừ|ứ|ử|ữ|ự/igm,"u");
+            a = a.replace(/\ /igm,"-");
+            $(".linklienket").text("http://pca.net.vn/" + a);   
+
+        }
+        else
+        {
         var a = $(this).val();
         a = a.replace(/à|á|ả|ã|ạ|ă|ằ|ắ|ẳ|ẵ|ặ|â|ầ|ấ|ẩ|ẫ|ậ/igm,"a");
         a = a.replace(/đ/igm, "d");
@@ -48,9 +77,18 @@ $(document).ready(function(){
         a = a.replace(/ù|ú|ủ|ũ|ụ|ư|ừ|ứ|ử|ữ|ự/igm,"u");
         a = a.replace(/\ /igm,"-");
         $(".linklienket").text("http://pca.net.vn/" + a);
+        }
     })
 
-/*Kiểm tra nếu radio dangNgay được check thì vô hiệu hóa trường .time và thiết lập giá trị là ngày hiện tại*/
+
+
+
+
+
+
+
+
+/*Kiểm tra nếu radio dangNgay được check thì vô hiệu hóa trường .time*/
     if($(".dangNgay").prop("checked")){
         $(".time").prop("disabled", true);
         var d = new Date();
@@ -85,12 +123,28 @@ $(document).ready(function(){
    
     $(".btnThem").click(function(){
         var tList = $(".theDanhDau").val();
-        var test =  $(".tagList").text($(".tagList").text()+tList + ", ");
+        if(tList != ""){
+        var test =  $(".tagList").text($(".tagList").text()+tList + " ");
+        }
+        $(".theDanhDau").val("");
+
     })
 
-/*Reset Liên kết*/
+/*Reset Liên kết + Thẻ*/
     $(".btnReset").click(function(){
-        $(".linklienket").text("");
+        $(".linklienket").text("http://pca.net.vn/");
+        $(".tagList").text("");
     })
+    var danhsach = [""];
+    // $(".btnThem").click(function(){
+        
+        
+    //     danhsach.push($(".theDanhDau").val());
+    //     $(".tagList").text(danhsach);
+    // })
+
+
+
+
 
 });
