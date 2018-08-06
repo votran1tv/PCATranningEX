46467967d46c3081a713c9ff8c1a1fcc1bb4f70e
Võ trần <40970433+votran1tv@users.noreply.github.com>
2018-08-06 12:31:38 +0700

    update sql-ex1 6/8

--- BAITAP/TRAN_VO/Database/SQLQuery_ẽ1.sql
+++ BAITAP/TRAN_VO/Database/SQLQuery_ẽ1.sql
@@ -17,14 +17,15 @@ IF  NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'Thanhvien')
         CREATE table Thanhvien(
 			id int identity,
 			taikhoan nvarchar(255),
-			matkhau nvarchar(255)
+			matkhau nvarchar(255),
+			primary key(id)
 			)
     END;
 	
 IF  NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'Dm_baiviet')
     BEGIN
         CREATE table Dm_baiviet(
-			id int identity,
+			id int primary key identity,
 			ten nvarchar(255)
 			)
     END;
@@ -34,8 +35,8 @@ IF  NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'Baiviet')
         CREATE table Baiviet(
 			id int identity,
 			tieude  nvarchar(255),
-			noidung text,
-			hinhanh text,
+			noidung ntext,
+			hinhanh ntext,
 			id_dm int
 			)
     END;
@@ -140,17 +141,116 @@ IF  NOT EXISTS (SELECT * FROM sysobjects WHERE name = 'Baiviet')
 	sp_rename 'Thanhvien', 'User'
 
 -- Chèn thêm cột cho bảng user
-	alter table [User] add Gender nvarchar(10)
-	alter table [User] add Email nvarchar(255)
-	alter table [User] add Phone varchar
+	alter table [User] add Gender ntext
+	alter table [User] add Email text
+	alter table [User] add Phone int
 	alter table [User] add Birthday date
-	alter table [User] add Country nvarchar(255)
+	alter table [User] add Country ntext
 
 -- Thêm dữ liệu cho cột mơi tạo
-	insert into [User] (Gender,Email, Phone, Birthday, Country) values (N'Nam','admin01@mail.com','123456','01/01/1990',N'Hà Nội');
+	set dateformat dmy;
+	update [User]
+	Set Gender='Nam', Email='admin001@mail.com', Phone='01234561', Birthday='01/01/1990', Country=N'Hà Nội'
+	Where id =1;
+	update [User]
+	Set Gender='Nam', Email='admin002@mail.com', Phone='01234562', Birthday='01/02/1990', Country=N'Nam Định'
+	Where id =2;
+	update [User]
+	Set Gender='Nam', Email='admin003@mail.com', Phone='01234563', Birthday='01/03/1990', Country=N'Lào Cai'
+	Where id =3;
+	update [User]
+	Set Gender='Nam', Email='admin004@mail.com', Phone='01234564', Birthday='01/04/1990', Country=N'Yên Bái'
+	Where id =4;
+	update [User]
+	Set Gender='Nam', Email='admin005@mail.com', Phone='01234565', Birthday='01/05/1990', Country=N'Hồ Chí Minh'
+	Where id =5;
+	update [User]
+	Set Gender='Nam', Email='admin006@mail.com', Phone='01234566', Birthday='01/06/1990', Country=N'Ninh Bình'
+	Where id =6;
+	update [User]
+	Set Gender='Nam', Email='admin007@mail.com', Phone='01234567', Birthday='01/07/1990', Country=N'Hòa Bình'
+	Where id =7;
+	update [User]
+	Set Gender='Nam', Email='admin008@mail.com', Phone='01234568', Birthday='01/08/1990', Country=N'Hà Nội2'
+	Where id =8;
+	update [User]
+	Set Gender='Nam', Email='admin009@mail.com', Phone='01234569', Birthday='01/09/1990', Country=N'Bình Định'
+	Where id =9;
+	update [User]
+	Set Gender='Nam', Email='admin010@mail.com', Phone='01234551', Birthday='01/10/1990', Country=N'Nghệ An'
+	Where id =10;
+	update [User]
+	Set Gender='Nam', Email='admin011@mail.com', Phone='01234552', Birthday='02/01/1990', Country=N'Hà Tĩnh'
+	Where id =11;
+	update [User]
+	Set Gender='Nam', Email='admin012@mail.com', Phone='01234553', Birthday='03/01/1990', Country=N'Long An'
+	Where id =12;
+	update [User]
+	Set Gender='Nam', Email='admin013@mail.com', Phone='01234554', Birthday='04/01/1990', Country=N'Quảng Ninh'
+	Where id =13;
+	update [User]
+	Set Gender='Nam', Email='admin014@mail.com', Phone='01234555', Birthday='05/01/1990', Country=N'Hải Phòng'
+	Where id =14;
+	update [User]
+	Set Gender='Nam', Email='admin015@mail.com', Phone='01234556', Birthday='06/01/1990', Country=N'Điện Biên'
+	Where id =15;
+	update [User]
+	Set Gender='Nam', Email='admin016@mail.com', Phone='01234557', Birthday='07/01/1990', Country=N'Cần Thơ'
+	Where id =16;
+	update [User]
+	Set Gender='Nam', Email='admin017@mail.com', Phone='01234558', Birthday='08/01/1990', Country=N'Hà Nội2'
+	Where id =17;
+	update [User]
+	Set Gender='Nam', Email='admin018@mail.com', Phone='01234559', Birthday='09/01/1990', Country=N'Phú Thọ'
+	Where id =18;
+	update [User]
+	Set Gender='Nam', Email='admin019@mail.com', Phone='01234570', Birthday='10/01/1990', Country=N'Nam Định'
+	Where id =19;
+	update [User]
+	Set Gender='Nam', Email='admin020@mail.com', Phone='01234571', Birthday='11/01/1990', Country=N'Hà Nội'
+	Where id =20;
+	update [User]
+	Set Gender='Nam', Email='admin021@mail.com', Phone='01234572', Birthday='12/01/1990', Country=N'Hà Nội3'
+	Where id =21;
+	update [User]
+	Set Gender='Nam', Email='admin022@mail.com', Phone='01234573', Birthday='13/01/1990', Country=N'Hà Nội4'
+	Where id =22;
+	update [User]
+	Set Gender='Nam', Email='admin023@mail.com', Phone='01234574', Birthday='14/01/1990', Country=N'Hà Nội5'
+	Where id =23;
+	update [User]
+	Set Gender='Nam', Email='admin024@mail.com', Phone='01234575', Birthday='15/01/1990', Country=N'Hà Nội6'
+	Where id =24;
+	update [User]
+	Set Gender='Nam', Email='admin025@mail.com', Phone='01234576', Birthday='16/01/1990', Country=N'Hà Nội7'
+	Where id =25;
+	update [User]
+	Set Gender='Nam', Email='admin026@mail.com', Phone='01234577', Birthday='17/01/1990', Country=N'Nam định'
+	Where id =26;
+	update [User]
+	Set Gender='Nam', Email='admin027@mail.com', Phone='01234578', Birthday='18/01/1990', Country=N'Long Khánh'
+	Where id =27;
+	update [User]
+	Set Gender='Nam', Email='admin028@mail.com', Phone='01234579', Birthday='19/01/1990', Country=N'Đà Nẵng'
+	Where id =28;
+	update [User]
+	Set Gender='Nam', Email='admin029@mail.com', Phone='01234580', Birthday='20/01/1990', Country=N'Cà Mau'
+	Where id =29;
+	update [User]
+	Set Gender='Nam', Email='admin030@mail.com', Phone='01234581', Birthday='20/01/1990', Country=N'Vũng Tàu'
+	Where id =30;
+	
+	select CONVERT(VARCHAR(10), GETDATE(), 103)
+
+
+ alter table Baiviet add constraint fkbaiviet  foreign key(id_dm) references Dm_Baiviet
+ alter table Baiviet with nocheck add constraint fkbaiviet  foreign key(id_dm) references Dm_Baiviet
+-- Tìm và xóa danh mục
+	ALTER TABLE Baiviet DROP CONSTRAINT fkbaiviet
+	delete from Dm_baiviet where ten=N'Thời sự';
 
 --Select bảng
-Select*from Thanhvien;
-Select*from Dm_baiviet;
-Select*from Baiviet;
-Select*from [dbo].[User];
+	Select*from Thanhvien;
+	Select*from Dm_baiviet;
+	Select*from Baiviet;
+	Select*from [dbo].[User];
